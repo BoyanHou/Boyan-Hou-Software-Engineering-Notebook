ae3fa8ce9d1bbd57fd07acbf055c24534a578eb4
Boyan Hou <boyan.hou@outlook.com>
2020-05-26 12:46:38 -0400

    Update get_composer.sh

--- Hack & HHVM/Composer/get_composer.sh
+++ Hack & HHVM/Composer/get_composer.sh
@@ -1,2 +1,2 @@
-$target_dir = /usr/local/bin
-echo "该脚本用于下载 composer.phar文件，并将其移入$PATH目录中（${target_dir}），使其可通过'composer', instead of 'php composer.phar'来调用"
+target_dir = /usr/local/bin
+echo "该脚本用于下载 composer.phar文件，并将其移入\$PATH目录中（${target_dir}），使其可通过'composer', instead of 'php composer.phar'来调用"
